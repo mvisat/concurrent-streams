bf3d0ed7ecdaa454198b04f187b9378b58430ad2
Muhamad Visat Sutarno <mvisat@gmail.com>
2019-01-13 21:25:47 +0700

    export options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { ConcurrentStream } from './stream';
+import { ReadStream, ReadStreamOptions } from './read-stream'
+import { WriteStream, WriteStreamOptions } from './write-stream';
 
 export { ConcurrentStream };
+export { ReadStream, ReadStreamOptions };
+export { WriteStream, WriteStreamOptions };
 export default ConcurrentStream;
