8ce963d9506757a94bafd4e7710f1c16afe268f3
Muhamad Visat Sutarno <mvisat@gmail.com>
2019-01-19 00:18:33 +0700

    no default export, re-export all class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
-import { ReadStream, ReadStreamOptions } from './read-stream';
-import { ConcurrentStream } from './stream';
-import { WriteStream, WriteStreamOptions } from './write-stream';
-
-export { ConcurrentStream };
-export { ReadStream, ReadStreamOptions };
-export { WriteStream, WriteStreamOptions };
-export default ConcurrentStream;
+export * from './read-stream';
+export * from './stream';
+export * from './write-stream';
